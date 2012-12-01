d0a8a4fffed1d57130fc773e1a9239fc9ef142ba
Justin Burris <justinbburris@gmail.com>
2012-12-01 12:44:04 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LuloConnect::Application.config.secret_token = 'a4f5dab94b900352f3453c39cd31efa95a4a2009db4b1c854bccea0559225ffcd39d80dc758630f27f911b734199cdeab9647bacf435a0c75507dd56c837961f'
