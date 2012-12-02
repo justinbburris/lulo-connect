8f825dfd806303c4b8d698377c7dca3f73d4526c
Justin Burris <justinbburris@gmail.com>
2012-12-01 21:52:46 -0500

    Ensure user is logged in (before creating farm) and save user to farm

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,7 +1,14 @@
 class FarmsController < InheritedResources::Base
+  before_filter :authenticate_user!, :only => [:new, :create, :destroy]
   before_filter do
     locale = params[:locale]
     Carmen.i18n_backend.locale = locale if locale
+  end
+
+  def create
+    @farm = Farm.new(params[:project])
+    @farm.user = current_user
+    create!
   end
 
 end
