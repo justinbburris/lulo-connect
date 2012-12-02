3aefa6a21a83ecbb47a26db2cb15a7c61e1123b2
David Bassendine <david@david-laptop.(none)>
2012-12-02 12:55:10 -0500

    Map tiles - use cloudmade rather than osm tiles

--- public/map.js
+++ public/map.js
@@ -1,13 +1,14 @@
 map = {
   initialize: function() {
     map = L.map('locations-map');
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    var apiKey = 'd9c90387c1314bc49a6922344ab43bc1';
+    L.tileLayer('http://{s}.tile.cloudmade.com/'+apiKey+'/997/256/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
       maxZoom: 18
     }).addTo(map);
     map.locate({setView: true, maxZoom: 12});
     map.on('locationfound', function(e) {
-      map.setZoom(12);
+      map.setZoom(13);
     });
   },
 
