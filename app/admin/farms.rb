fccd131e4e3283d9d13eb6e3d6c2ebfeb16dec0c
Justin Burris <justinbburris@gmail.com>
2012-12-02 12:09:44 -0500

    Administrate farms in active admin

--- app/admin/farms.rb
+++ app/admin/farms.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Farm do
-  
+  form do |f|
+    f.inputs "Basics" do
+      f.input :name
+      f.input :street
+      f.input :city
+      f.input :state
+      f.input :zip
+      f.input :volunteers_needed
+      f.input :website
+      f.input :latitude
+      f.input :longitude
+    end
+    f.inputs "Extra" do
+      f.input :crops
+      f.input :growing_season
+      f.input :hours_of_operation
+      f.input :type_of_farm
+      f.input :size_in_acres
+    end
+    f.buttons
+  end
 end
